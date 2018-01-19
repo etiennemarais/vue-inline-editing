9391bf84c87db9c58185e2c3430bd0e2c6260a15
Etienne Marais <etienne.m@superbalist.com>
2018-01-19 16:53:18 +0200

    Renamed public to docs to enable GH pages better

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix').mix;
 
-mix.js('src/js/index.js', 'public/js')
-    .copy('src/index.html', 'public/index.html')
-    .sass('src/scss/app.scss', 'public/css')
+mix.js('src/js/index.js', 'docs/js')
+    .copy('src/index.html', 'docs/index.html')
+    .sass('src/scss/app.scss', 'docs/css')
     .disableNotifications();
