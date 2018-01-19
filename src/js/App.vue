b4d19a922ae00a7ab5ca4d67aa9bcc8b292f227a
Etienne Marais <etienne.m@superbalist.com>
2018-01-19 13:43:01 +0200

    Inline editing component by using vue editor and conditional rendering along with vmodel binding

--- src/js/App.vue
+++ src/js/App.vue
@@ -6,6 +6,13 @@
                     <div class="columns">
                         <div class="column is-full">
                             <h1 class="title is-1">Inline components</h1>
+                            <div v-if="isBeingEdited">
+                                <vue-editor :editorToolbar="customToolbar" v-model="content"></vue-editor>
+                                <a class="button" v-on:click="isBeingEdited = false">Save</a>
+                            </div>
+                            <div v-else="isBeingEdited" v-on:click="isBeingEdited = true">
+                                <p v-html="content"></p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -16,12 +23,21 @@
 </template>
 
 <script>
+import { VueEditor } from 'vue2-editor'
+
 export default {
     name: "App",
-    components: {},
+    components: {
+        VueEditor
+    },
     data() {
         return {
-
+            content: "This is the initial paragraph to render and if you click on this you should be able to edit it",
+            isBeingEdited: false,
+            customToolbar: [
+                ['bold', 'italic', 'underline'],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }]
+            ]
         }
     },
     methods: {
@@ -30,7 +46,7 @@ export default {
         }
     },
     created () {
-        this.init();
+        this.init()
     }
 };
 </script>
